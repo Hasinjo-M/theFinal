3333d109866ae47462ff7c205c7deafd934a0424
Hasinjo-M <118592092+Hasinjo-M@users.noreply.github.com>
2024-02-12 21:44:05 -0800

    menu

--- pages/index.php
+++ pages/index.php
@@ -10,20 +10,40 @@ $page = isset($_GET['page']) ? $_GET['page'] : "login";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/bootstrap-3.3.6-dist/css/bootstrap.css">
     <script src="../assets/bootstrap-3.3.6-dist/js/jquery.js"></script>
+    <title>Your Page Title</title>
 </head>
 
 <body>
-    <div>
-        <a href="index.php?page=backoffice/Accueil">Variete the</a>
-        <a href="index.php?page=backoffice/Parcelle"> Parcelle</a>
-        <a href="index.php?page=backoffice/Cueilleur"> Cueilleur</a>
-        <a href="index.php?page=backoffice/Depenses"> Categorie depense</a>
-        <a href="index.php?page=backoffice/Salaire"> Salaire</a>
-        
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Thé</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li <?php echo ($page == 'backoffice/Accueil') ? 'class="active"' : ''; ?>>
+                            <a href="index.php?page=backoffice/Accueil">Variete thé</a>
+                        </li>
+                        <li <?php echo ($page == 'backoffice/Parcelle') ? 'class="active"' : ''; ?>>
+                            <a href="index.php?page=backoffice/Parcelle">Parcelle</a>
+                        </li>
+                        <li <?php echo ($page == 'backoffice/Cueilleur') ? 'class="active"' : ''; ?>>
+                            <a href="index.php?page=backoffice/Cueilleur">Cueilleur</a>
+                        </li>
+                        <li <?php echo ($page == 'backoffice/Depenses') ? 'class="active"' : ''; ?>>
+                            <a href="index.php?page=backoffice/Depenses">Catégorie dépense</a>
+                        </li>
+                        <li <?php echo ($page == 'backoffice/Salaire') ? 'class="active"' : ''; ?>>
+                            <a href="index.php?page=backoffice/Salaire">Salaire</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <?php include($page . '.php') ?>
     </div>
-    <script src=".../assets/bootstrap-3.3.6-dist/js/jquery.js"></script>
+    <script src="../assets/bootstrap-3.3.6-dist/js/jquery.js"></script>
     <script src="../assets/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 </body>
-
 </html>
