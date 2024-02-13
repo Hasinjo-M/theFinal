b5a06cc0f3e92943d9d3472d7bf402950022ec24
Hasinjo-M <118592092+Hasinjo-M@users.noreply.github.com>
2024-02-12 22:14:40 -0800

    debutFront

---
+++ pages/indexFront.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$page = isset($_GET['page']) ? $_GET['page'] : "login";
+
+if ((!isset($_SESSION['role']) || !isset($_SESSION['idUtilisateur'])) && $page != 'loginFront') {
+    header("Location: loginFront.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/bootstrap-3.3.6-dist/css/bootstrap.css">
+    <script src="../assets/bootstrap-3.3.6-dist/js/jquery.js"></script>
+    <title>Thé</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Thé</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li <?php echo ($page == 'backoffice/Cueillettes') ? 'class="active"' : ''; ?>>
+                            <a href="indexFront.php?page=backoffice/Cueillettes">Cueillettes</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <?php include($page . '.php') ?>
+    </div>
+    <script src="../assets/bootstrap-3.3.6-dist/js/jquery.js"></script>
+    <script src="../assets/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
--- pages/loginFront.php
+++ pages/loginFront.php
@@ -11,9 +11,9 @@
                 $_SESSION['role'] = $adm['role'];
                 $_SESSION['id'] = $adm['idadmin'];
             } 
-            header("Location: indexFront.php?page=frontoffice/Accueil");
+            header("Location: index.php?page=backoffice/Accueil");
         }else{
-            header("location: indexFront.php?page=loginFront&erreur=Il y a une erreur");
+            header("location: index.php?page=login&erreur=Il y a une erreur");
         }
        
         exit;    
@@ -31,15 +31,15 @@
         <div class="shape col-md-6"></div>
     </div>
     <form  class="col-md-4" action="login.php" method="POST" >
-        <h3>Login Administateur</h3>
+        <h3>Login Utilisateur</h3>
         <label for="#" style="Color :RED;" ><?php
             if(isset($_GET['erreur'])){echo $erreur;}
         ?></label>
         <label for="username">Email</label>
-        <input type="text" name="email" placeholder="Email " value="admin@gmail.com" id="username" required>
+        <input type="text" name="email" placeholder="Email " value="rabe@example.com" id="username" required>
 
         <label for="password">Password</label>
-        <input type="password" name="pwd" placeholder="Password" value="admin123" id="password" required>
+        <input type="password" name="pwd" placeholder="Password" value="password" id="password" required>
 
         <input class="button" type="submit" value="Log In">
         
